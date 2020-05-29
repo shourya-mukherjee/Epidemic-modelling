3b6c0190e22e9c77cd3687eb8573f458899ff164
shouryam <smukherjee@lucylabs.io>
2020-05-29 00:10:53 -0700

    fleshed out Graph class, nx doesnt have communicability centrality for some reason

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 class Population:
 	def __init__(self, numvertices, numedges):
@@ -22,32 +23,59 @@ class Graph(object):
 	def __init__(self, n, edge_prob):
 		self.n = n
 		self.graph = nx.fast_gnp_random_graph(n=n, p=edge_prob)
-		self.avg_deg_cen = avg_degree_centrality()
+		nx.set_node_attributes(self.graph, 0, 'state')
 
-	def rand_infect():
-		pass
+		self.avg_deg_cen = self.avg_degree_centrality()
+		#self.avg_com_cen = self.avg_communicability_centrality()
+		self.avg_ecc = self.avg_eccentricity()
+		self.diameter = nx.diameter(self.graph)
 
-	def avg_degree_centrality():
+		self.infected_vertices = []
+		self.color_map = []
+		self.rand_infect()
+
+	def rand_infect(self):
+		for i in range(self.n):
+			flip = random.randint(0, 1)
+			if flip:
+				self.graph.nodes[i]['state'] = 1
+				self.infected_vertices.append(i)
+				self.color_map.append('red')
+			else:
+				self.color_map.append('green')
+
+	def avg_degree_centrality(self):
 		"""
 		The degree centrality for a node v is the fraction of nodes it is connected to.
 		"""
 		return sum(nx.degree_centrality(self.graph).values())/self.n
 
-	def avg_communicability_centrality():
+	def avg_communicability_centrality(self):
 		"""
 		Communicability centrality, also called subgraph centrality, of a node n is the sum of closed walks of all lengths starting and ending at node n.
 		"""
 		return sum(nx.communicability_centrality(self.graph).values())/self.n
 
+	def avg_eccentricity(self):
+		"""
+		The eccentricity of a node v is the maximum distance from v to all other nodes in G.
+		"""
+		return sum(nx.eccentricity(self.graph).values())/self.n
+
+	def node_state(self, node):
+		state = self.graph.nodes[node]['state']
+		if state == 0:
+			print ("Not infected")
+		elif state == 1:
+			print ("Infected")
+		else:
+			print ("Recovered")
 
 def test():
 	G = Graph(50, 0.3543539)
-	print (nx.degree_centrality(G.graph))
-	print (sum(nx.degree_centrality(G.graph).values())/50)
-
 
 	plt.subplot(121)
-	nx.draw(G.graph, with_labels=True, font_weight='bold')
+	nx.draw(G.graph, with_labels=True, font_weight='bold', node_color=G.color_map)
 	plt.show()
 
 if __name__ == '__main__':
